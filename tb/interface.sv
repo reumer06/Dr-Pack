4b236dcb83a3874f825254d47203f3ba8b11ed52
Umer Shaikh <195533734+reumer06@users.noreply.github.com>
2026-08-22 05:22:29 +0530

    feat: add interface

---
+++ tb/interface.sv
@@ -0,0 +1,9 @@
+interface router(input bit clk);
+    logic        reset       ;
+    logic        isValid     ;
+    logic [1: 0] inAddr      ;
+    logic  [7:0] inData      ;
+
+    logic  [7:0] outData  [4];
+    logic        outValid [4];
+endinterface
