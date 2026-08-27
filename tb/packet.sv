22e189eeb699739f3a5e837c9ddee7da39ab7616
Umer Shaikh <195533734+reumer06@users.noreply.github.com>
2026-08-28 00:44:42 +0530

    refactor: replace SV rand with procedural randomization

--- tb/packet.sv
+++ tb/packet.sv
@@ -1,10 +1,11 @@
 class Packet;
-    rand bit [1:0] destination;
-    rand bit [7:0] payload;
+    bit [1:0] destination;
+    bit [7:0] payload;
 
-    constraint validDest {
-        destination inside {[0:3]};
-    }
+    function void randomizePkt();
+        this.destination = $urandom_range(0, 3);
+        this.payload     = $urandom();
+    endfunction
 
     function void display(string tag);
         $display("[%s] Destination port: %0d | Payload: 0x%0h", tag, destination, payload);
